7c4e9d6cacd04cc382a71a1e89a7f2393ab5eff2
Christian Astudillo <christianandres.as@gmail.com>
2020-06-12 10:00:35 -0400

    no message

---
+++ class/persona.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello word</title>
+        
+        <?php
+            echo "Hello Hord";
+        ?>
+    </head>
+    <body>
+    </body>
+</html>
